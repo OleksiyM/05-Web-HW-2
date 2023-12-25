a9934dff02349e8242a89c79d8bbc39322d69178
OleksiyM <OleksiyMaliovanyy@gmail.com>
2023-12-25 13:51:45 +0200

    fix: crash when sending empty message

--- Server.py
+++ Server.py
@@ -39,29 +39,30 @@ class Server:
 
     async def distribute(self, ws: WebSocketServerProtocol):
         async for message in ws:
-            is_exchange, rates_days, currencies = await check_exchange_message(message)
-            await self.send_to_clients(f"{ws.name}: {message}")
-            if is_exchange:
-                logging.debug(f'{rates_days=}, {currencies=}')
-                await handle_exchange(
-                    f'{ws.name}: {message}; Request handled for current day and {rates_days} archive days and {currencies}')
+            if message:
+                is_exchange, rates_days, currencies = await check_exchange_message(message)
+                await self.send_to_clients(f"{ws.name}: {message}")
+                if is_exchange:
+                    logging.debug(f'{rates_days=}, {currencies=}')
+                    await handle_exchange(
+                        f'{ws.name}: {message}; Request handled for current day and {rates_days} archive days and {currencies}')
 
-                await self.send_to_clients(
-                    f'Server: getting rates from Privat Bank for current day and {rates_days} archive days for {currencies}')
+                    await self.send_to_clients(
+                        f'Server: getting rates from Privat Bank for current day and {rates_days} archive days for {currencies}')
 
-                result = await get_data(rates_days, currencies)
-                html_table = await generate_html_table(result)
-                # html_table_m = await generate_html_table_1(result)
-                # decoded_html_table = html.unescape(html_table)
+                    result = await get_data(rates_days, currencies)
+                    html_table = await generate_html_table(result)
+                    # html_table_m = await generate_html_table_1(result)
+                    # decoded_html_table = html.unescape(html_table)
 
-                # text formatted table
-                # text_table = await generate_text_table_html(result)
-                # for l in text_table:
-                #     await self.send_to_clients(l)
-                await self.send_to_clients('<h3>Exchange rates</h3>\n')
-                await self.send_to_clients(html_table)
-                # await self.send_to_clients(html_table_m)
-                # await self.send_to_clients(decoded_html_table)
-                # logging.debug(html_table)
-                # logging.debug(text_table)
-            # await self.send_to_clients(f"{ws.name}: {message}")
+                    # text formatted table
+                    # text_table = await generate_text_table_html(result)
+                    # for l in text_table:
+                    #     await self.send_to_clients(l)
+                    await self.send_to_clients('<h3>Exchange rates</h3>\n')
+                    await self.send_to_clients(html_table)
+                    # await self.send_to_clients(html_table_m)
+                    # await self.send_to_clients(decoded_html_table)
+                    # logging.debug(html_table)
+                    # logging.debug(text_table)
+                # await self.send_to_clients(f"{ws.name}: {message}")
